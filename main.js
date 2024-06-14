2aab3292235cb395d95d8006eb8b5fbe50168637
Constant-ha <kkumantsov@gmail.com>
2024-06-14 19:21:11 +0600

    Create a markup for Bingo cards generator #4  {another select on js, card 3x3, fullsreen}

--- main.js
+++ main.js
@@ -9,7 +9,25 @@ optionItems.forEach((item) => {
   });
 });
 
-
-button.addEventListener("click", () => {
+button.addEventListener("click", function () {
   list.classList.toggle("dropup__list--visible");
 });
+
+
+
+const button1 = document.querySelector(".dropup__button1");
+const list1 = document.querySelector(".dropup__list1");
+const optionItems1 = document.querySelectorAll(".dropup__list-item1");
+
+optionItems1.forEach((item) => {
+  item.addEventListener("click", () => {
+    button1.innerHTML = item.innerText;
+    list1.classList.toggle("dropup__list--visible1");
+  });
+});
+
+button1.addEventListener("click", function () {
+  list1.classList.toggle("dropup__list--visible1");
+});
+
+
