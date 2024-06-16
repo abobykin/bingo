1470842cdfafcd1433c4087e3d653d2f59de6484
Constant-ha <kkumantsov@gmail.com>
2024-06-16 23:18:30 +0600

    Create a markup for Bingo cards generator #5  {several content for js functions}

--- main.js
+++ main.js
@@ -1,33 +1,73 @@
-const button = document.querySelector(".dropup__button");
-const list = document.querySelector(".dropup__list");
-const optionItems = document.querySelectorAll(".dropup__list-item");
+const gridLayoutButton = document.querySelector(".grid-layout__button");
+const gridLayoutList = document.querySelector(".grid-layout__list");
+const gridLayoutItem = document.querySelectorAll(".grid-layout__item");
+const cardInner = document.querySelector(".card__inner")
 
-optionItems.forEach((item) => {
+gridLayoutItem.forEach((item) => {
   item.addEventListener("click", () => {
-    button.innerHTML = item.innerText;
-    list.classList.toggle("dropup__list--visible");
+    gridLayoutButton.innerHTML = item.innerText;
+    gridLayoutList.classList.toggle("dropup__list--visible");
+    if ( item.innerText === '3x3'  ) {
+      cardInner.classList.add('card__inner--three')
+      
+    } else {
+      cardInner.classList.remove('card__inner--three')
+    }
+    
   });
 });
 
-button.addEventListener("click", function () {
-  list.classList.toggle("dropup__list--visible");
+gridLayoutButton.addEventListener("click", function () {
+  gridLayoutList.classList.toggle("dropup__list--visible");
 });
 
 
 
-const button1 = document.querySelector(".dropup__button1");
-const list1 = document.querySelector(".dropup__list1");
-const optionItems1 = document.querySelectorAll(".dropup__list-item1");
+const cellCizeButton = document.querySelector(".cell-size__button");
+const cellCizeList = document.querySelector(".cell-size__list");
+const cellSizeItem = document.querySelectorAll(".cell-size__item");
+const cardGame = document.querySelector('.card-game');
 
-optionItems1.forEach((item) => {
+
+cellSizeItem.forEach((item) => {
   item.addEventListener("click", () => {
-    button1.innerHTML = item.innerText;
-    list1.classList.toggle("dropup__list--visible1");
+    cellCizeButton.innerHTML = item.innerText;
+    cellCizeList.classList.toggle("dropup__list--visible");
+    if ( item.innerText === 'full screen'  ) {
+      cardGame.classList.add('card-game-full')
+      
+    } else {
+      cardGame.classList.remove('card-game-full')
+    }
   });
 });
 
-button1.addEventListener("click", function () {
-  list1.classList.toggle("dropup__list--visible1");
+cellCizeButton.addEventListener("click", function () {
+  cellCizeList.classList.toggle("dropup__list--visible");
 });
+
+
+
+
+const coundDownEl = document.getElementById('countdown'); 
+let time = 600;
+setInterval(updateCountdown, 1000);
+
+
+function updateCountdown() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60; 
+  seconds = seconds < 10 ? "0" + seconds:
+  seconds;
+  coundDownEl.innerHTML =  `${minutes}: ${seconds} `;
+  time -- ;
+}
+
+
+
+
+
+
+
 
 
