f8cebb2fb0afc1e0c77e2f7bb72eed7123c17741
Constant-ha <kkumantsov@gmail.com>
2024-06-20 13:44:51 +0600

    Create a markup for Bingo cards generator #6 {cycle inside the gridLayoutButton and other content}

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
+// switch gridlayout to 3x3
 const gridLayoutButton = document.querySelector(".grid-layout__button");
 const gridLayoutList = document.querySelector(".grid-layout__list");
 const gridLayoutItem = document.querySelectorAll(".grid-layout__item");
 const cardInner = document.querySelector(".card__inner")
+const cards = document.querySelectorAll('.card__item');
+
 
 gridLayoutItem.forEach((item) => {
   item.addEventListener("click", () => {
     gridLayoutButton.innerHTML = item.innerText;
     gridLayoutList.classList.toggle("dropup__list--visible");
-    if ( item.innerText === '3x3'  ) {
-      cardInner.classList.add('card__inner--three')
-      
-    } else {
-      cardInner.classList.remove('card__inner--three')
-    }
-    
+    if ( item.innerText === '3x3' ) {
+      cardInner.classList.add('card__inner--three');
+      cards.forEach((card, index) => {
+        if (index > 8) {
+          card.remove();      
+        }
+      });      
+    } else {      
+      cardInner.classList.remove('card__inner--three');
+      const cardItem = document.createElement('div');
+      cardItem.className = 'card__item';
+      const cardImg = document.createElement('img');  
+      cardImg.className = 'card__item-img';
+      cardImg.src = '/images/card-img.png';
+      cardItem.appendChild(cardImg);
+      for (let i = 0; i < 17; i++) { 
+        cardInner.appendChild(cardItem);
+      }    
+
+    }   
   });
 });
 
@@ -22,7 +38,7 @@ gridLayoutButton.addEventListener("click", function () {
 });
 
 
-
+// switch gridlayout to fullscreen
 const cellCizeButton = document.querySelector(".cell-size__button");
 const cellCizeList = document.querySelector(".cell-size__list");
 const cellSizeItem = document.querySelectorAll(".cell-size__item");
@@ -47,21 +63,114 @@ cellCizeButton.addEventListener("click", function () {
 });
 
 
+const startIn = document.getElementById('startin');
+let timeIn = 3;
+
+function timeInCount() {
+  startIn.innerText = timeIn;
+  
+  if (timeIn != 0) {
+    timeIn --;
+    
+  } else {
+    const startTimer = document.querySelector('.start-timer')
+    startTimer.classList.add('start-timer--active');
+    const bingoStartIn = document.querySelector('.bingo-startin')
+    bingoStartIn.classList.remove('bingo-startin--active')
+
+
+  }
+  
+  
+};
+  // play function
+// setInterval(timeInCount, 1000);
+
+
+
+
+
 
 
 const coundDownEl = document.getElementById('countdown'); 
-let time = 600;
+let timer = 600;
+
 setInterval(updateCountdown, 1000);
 
-
 function updateCountdown() {
-  const minutes = Math.floor(time / 60);
-  let seconds = time % 60; 
+  const minutes = Math.floor(timer / 60);
+  let seconds = timer % 60; 
   seconds = seconds < 10 ? "0" + seconds:
   seconds;
-  coundDownEl.innerHTML =  `${minutes}: ${seconds} `;
-  time -- ;
-}
+  coundDownEl.innerHTML =  `${minutes }: ${seconds} `;
+  timer -- ;
+};
+
+const chores = [
+  'Do the laundry',
+  'Take out the trash',
+  'Mow the lawn',
+  'Do the dishes',
+  'Wash the car',
+  'Wash the carpet',
+  'Clean the windows',
+  'Dust the furniture',
+];
+
+const randomChore = chores[Math.floor(Math.random() * chores.length)]; 
+const buttonGenerate = document.querySelector('.bingo-form__generate');
+const buttonStart = document.querySelector('.bingo-form__start');
+
+
+
+
+buttonGenerate.addEventListener('click', function () {
+
+    // this is 0 point of plan
+      chores.forEach((chore, index ) => {
+        buttonGenerate.classList.add('bingo-form__disabled');
+        buttonGenerate.classList.remove('bingo-form__active');
+      
+        buttonStart.classList.remove('bingo-form__disabled');
+        buttonStart.classList.add('bingo-form__active');
+          
+        
+          setTimeout(function() {
+          const randomCard = cards[Math.floor(Math.random() * cards.length)];
+          const para = document.createElement('p');
+          para.className = 'card__item-text';
+          const text = document.createTextNode(chore);
+          para.appendChild(text);     
+          
+          // this is 1 point of plan
+          if (!randomCard.classList.contains('card__item--active')) {     
+              randomCard.classList.add('card__item--active');
+              randomCard.appendChild(para); 
+              console.log(chore);  } 
+          else {
+              return; 
+          }
+        }, index * 350)
+        
+    
+        });
+
+          
+      
+    }); 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
